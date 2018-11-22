5c6125c33a673b29876c61b327ca479a9f3c27ad
VLC <valentinleonchristensen@gmail.com>
2018-11-22 16:02:14 +0100

    dummy mockup of HTML

--- src/main/java/firstyear/project/Aloha.java
+++
@@ -1,4 +0,0 @@
-package firstyear.project;
-
-public class Aloha {
-}
---
+++ src/main/java/firstyear/project/CalendarController.java
@@ -0,0 +1,18 @@
+package firstyear.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class CalendarController {
+    private final String INDEX = "index";
+
+    @GetMapping("/")
+    public String index(Model model){
+        return INDEX;
+    }
+
+}
