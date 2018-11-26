8f06fa86501389055086d3d388d4cfdcf176c69c
VLC <valentinleonchristensen@gmail.com>
2018-11-26 12:24:37 +0100

    changed constants to a working database

--- src/main/java/firstyear/project/repositories/JdbcFix.java
+++ src/main/java/firstyear/project/repositories/JdbcFix.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcFix {
-    private final String url = "jdbc:mysql://firstyearproject.cqw1hsl5y1ft.eu-central-1.rds.amazonaws.com:3306/charlie?useSSL=false";
-    private final String username = "firstyearproject";
-    private final String password = "firstyearpassword";
+    private final String url = "jdbc:mysql://charlie.cgkrzn2ampmn.eu-central-1.rds.amazonaws.com:3306/charlie?useSSL=false";
+    private final String username = "myfinger";
+    private final String password = "qisig2018";
     protected Connection connection = null;
 
     public JdbcFix(){
