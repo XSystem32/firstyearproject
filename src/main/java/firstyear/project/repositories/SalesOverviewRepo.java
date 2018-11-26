2ff308e477fb8f2620bfe167b99e66fb2df897b8
Jonathan Laage Olsen <jonathan.laage.olsen@gmail.com>
2018-11-26 11:53:14 +0100

    Repo tilført

--- src/main/java/firstyear/project/repositories/SalesOverviewRepo.java
+++ src/main/java/firstyear/project/repositories/SalesOverviewRepo.java
@@ -1,7 +1,14 @@
 package firstyear.project.repositories;
 
+import firstyear.project.models.SalesOverview;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface SalesOverviewRepo {
+
+    boolean createSalesOverview(SalesOverview salesOverview);
+    boolean deleteSalesOverview(int index);
+    boolean updateSalesOverview(SalesOverview salesOverview);
+    SalesOverview getSalesOverview(int index);
 }
