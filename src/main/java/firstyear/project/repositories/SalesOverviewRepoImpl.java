1108cc49efd7456f5143c70cb46cd376fe8b23be
VLC <valentinleonchristensen@gmail.com>
2018-11-26 13:05:42 +0100

    implemented the salesrepo interface on service and repoimpl

--- src/main/java/firstyear/project/repositories/SalesOverviewRepoImpl.java
+++ src/main/java/firstyear/project/repositories/SalesOverviewRepoImpl.java
@@ -1,7 +1,28 @@
 package firstyear.project.repositories;
 
+import firstyear.project.models.SalesOverview;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class SalesOverviewRepoImpl implements SalesOverviewRepo{
+
+    @Override
+    public boolean createSalesOverview(SalesOverview salesOverview) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteSalesOverview(int index) {
+        return false;
+    }
+
+    @Override
+    public boolean updateSalesOverview(SalesOverview salesOverview) {
+        return false;
+    }
+
+    @Override
+    public SalesOverview getSalesOverview(int index) {
+        return null;
+    }
 }
--- src/main/java/firstyear/project/services/SalesOverviewService.java
+++ src/main/java/firstyear/project/services/SalesOverviewService.java
@@ -1,7 +1,29 @@
 package firstyear.project.services;
 
+import firstyear.project.models.SalesOverview;
+import firstyear.project.repositories.SalesOverviewRepo;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SalesOverviewService {
+public class SalesOverviewService implements SalesOverviewRepo {
+
+    @Override
+    public boolean createSalesOverview(SalesOverview salesOverview) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteSalesOverview(int index) {
+        return false;
+    }
+
+    @Override
+    public boolean updateSalesOverview(SalesOverview salesOverview) {
+        return false;
+    }
+
+    @Override
+    public SalesOverview getSalesOverview(int index) {
+        return null;
+    }
 }
