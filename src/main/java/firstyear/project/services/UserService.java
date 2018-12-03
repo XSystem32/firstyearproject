7e8b20922e6074841c69ba48edb2d3197b70e765
Jonathan Laage Olsen <jonathan.laage.olsen@gmail.com>
2018-12-03 16:25:37 +0100

    user service

--- src/main/java/firstyear/project/services/UserService.java
+++ src/main/java/firstyear/project/services/UserService.java
@@ -2,34 +2,44 @@ package firstyear.project.services;
 
 import firstyear.project.models.User;
 import firstyear.project.repositories.userRepo.UserRepo;
+import firstyear.project.repositories.userRepo.UserRepoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService implements UserRepo {
+
+    @Autowired
+    UserRepoImpl userRepo;
+
     @Override
     public boolean createUser(User user) {
+        userRepo.createUser(user);
         return false;
     }
 
     @Override
     public boolean deleteUser(int index) {
-        return false;
+        userRepo.deleteUser(index);
+        return true;
     }
 
     @Override
     public boolean updateUser(int index, User user) {
-        return false;
+        userRepo.updateUser(index, user);
+        return true;
     }
 
     @Override
     public User getUser(int index) {
-        return null;
+
+        return userRepo.getUser(index);
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        return userRepo.getUsers();
     }
 }
