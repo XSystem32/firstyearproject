657b1ecd5204b47506d0cfe50313542b8c69a4b2
VLC <valentinleonchristensen@gmail.com>
2018-12-03 15:24:24 +0100

    made the basics of the html page for user overview

--- src/main/java/firstyear/project/controllers/userController/UserController.java
+++ src/main/java/firstyear/project/controllers/userController/UserController.java
@@ -1,10 +1,12 @@
 package firstyear.project.controllers.userController;
 
 import firstyear.project.controllers.salesOverviewController.SalesOverviewController;
+import firstyear.project.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 @Controller
@@ -15,7 +17,8 @@ public class UserController {
     private final String CREATE_USER = "user/createUser.html";
     private final String DELETE_USER = "user/deleteUser.html";
     private final String UPDATE_USER = "user/updateUser.html";
-    private final String DISPLAY_USER = "user/displayUser.html";
+    private final String DISPLAY_USER = "user/User.html";
+    private final String DISPLAY_USERS = "user/Users.html";
     private final String REDIRECT_INDEX = "redirect:/";
 
     @GetMapping("user/createUser")
@@ -47,6 +50,21 @@ public class UserController {
     public String display(Model model) {
         LOGGER.info("create was called... ");
         return DISPLAY_USER;
+    }
+
+    @GetMapping("user/displayUsers")
+    public String displayUsers(Model model) {
+        LOGGER.info("create was called... ");
+
+        ArrayList<User> users = new ArrayList<>();
+
+        users.add(new User(1,"ptrprkt","asdf","peter@parker.dk","Peter Parker", "1"));
+        users.add(new User(1,"katiee","asdf","katie@parker.dk","Katie Parker", "1"));
+        users.add(new User(1,"coolcat","asdf","catt@parker.dk","Frank Black", "1"));
+
+
+        model.addAttribute("users", users);
+        return DISPLAY_USERS;
     }
 
 }
