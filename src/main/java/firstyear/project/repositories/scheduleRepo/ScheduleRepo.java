3d944df2ffdce48ff68c5db49481dc0e4a7272b2
Jonathan Laage Olsen <jonathan.laage.olsen@gmail.com>
2018-12-11 15:45:37 +0100

    change repo til et repository

--- src/main/java/firstyear/project/repositories/scheduleRepo/ScheduleRepo.java
+++ src/main/java/firstyear/project/repositories/scheduleRepo/ScheduleRepo.java
@@ -2,10 +2,12 @@ package firstyear.project.repositories.scheduleRepo;
 
 import firstyear.project.models.Schedule;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalTime;
 import java.util.List;
 
+@Repository
 public interface ScheduleRepo {
 
     boolean createSchedule (Schedule schedule);
